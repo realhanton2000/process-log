127503dc3da77d11b4fb07768b1c0e535d1b781a
hongtao <realhanton@gmail.com>
2021-03-31 18:59:04 +0100

    minor change

--- src/main/java/processlog/logic/EventPool.java
+++ src/main/java/processlog/logic/EventPool.java
@@ -16,9 +16,9 @@ public class EventPool {
 
     private Logger logger = LoggerFactory.getLogger(EventPool.class);
 
-    @Value("${processlog.writeTrigger:10}")
+    @Value("${processlog.writeTrigger:100}")
     private int writeTrigger;
-    @Value("${processlog.maxSize:100}")
+    @Value("${processlog.maxSize:-1}")
     private int maxSize;
 
     private volatile Map<String, EventPair> all = new HashMap<>();
@@ -67,7 +67,7 @@ public class EventPool {
         if (paired.size() >= writeTrigger) {
             writeAndFlushPaired();
         }
-        //disable size check with maxSize is not positive value
+        //disable size check when maxSize is not positive value
         if (maxSize > 0 && all.size() >= maxSize) {
             writeAndFlushPaired();
             all.clear();
